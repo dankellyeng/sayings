c572e2d5fe9baab09836772c7aa451f1eefe717a
dankellyeng <danielkelly27@gmail.com>
2023-12-23 17:20:32 +1100

    removed unused code

--- app/components/sayingBody/SayingBody.tsx
+++ app/components/sayingBody/SayingBody.tsx
@@ -1,21 +1,10 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import sayingBody from "./sayingBody.module.scss";
-import { Button } from "@radix-ui/themes";
 import SayingCards from "../sayingCard/SayingCards";
 import SayingGenerator from "../sayingGenerator/SayingGenerator";
 
 const SayingCard: React.FunctionComponent<any> = ({ data }) => {
-  const [hardCoverOnly, setHardCoverOnly] = useState(false);
-
-  const handleHardCoverClick = () => {
-    setHardCoverOnly(true);
-  };
-
-  const handleGetBooksClick = () => {
-    setHardCoverOnly(false);
-  };
-
   return (
     <div className={sayingBody.body}>
       <div className={sayingBody.books}>
