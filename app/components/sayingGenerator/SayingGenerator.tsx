f628714840020b22a89743c34ca9a4ea216b903e
dankellyeng <danielkelly27@gmail.com>
2023-12-23 13:59:11 +1100

    added sayingGenerator and styled

--- app/components/sayingGenerator/SayingGenerator.tsx
+++ app/components/sayingGenerator/SayingGenerator.tsx
@@ -1,33 +1,55 @@
 "use client";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import sayingGenerator from "./sayingGenerator.module.scss";
+import { Button, Text } from "@radix-ui/themes";
+import Image from "next/image";
 
 interface Props {
   data: any[];
 }
 
-function sortArray(arr: BookType[]) {
-  arr.sort(function (a, b) {
-    const textA = a.name.toUpperCase();
-    const textB = b.name.toUpperCase();
-    return textA < textB ? -1 : textA > textB ? 1 : 0;
-  });
-}
-
 const SayingGenerator: FC<Props> = ({ ...props }) => {
+  const [generatedSaying, setGeneratedSaying] = useState("");
   if (!props) {
     return;
+  }
+
+  function generateSaying() {
+    setGeneratedSaying(
+      sayings[Math.floor(Math.random() * sayings.length)].saying
+    );
   }
 
   const sayings = props.data;
 
   return (
     <div className={sayingGenerator.allCards}>
-      <p className={sayingGenerator.heading}>Top 1 Sayings</p>
-
-      {sayings.map((saying, index) => {
-        return <p key={index}>{`${index + 1}. ${saying.saying}`}</p>;
-      })}
+      {generatedSaying !== "" ? (
+        <div className={sayingGenerator.speechBubble}>
+          <p className={sayingGenerator.saying}>{generatedSaying}</p>
+        </div>
+      ) : (
+        <div className={sayingGenerator.placeholder} />
+      )}
+      <div className={sayingGenerator.imageContainer}>
+        <Image
+          className={sayingGenerator.image1}
+          fill={true}
+          src="/images/circle.svg"
+          alt="Circle"
+        />
+        <Image
+          className={sayingGenerator.image2}
+          fill={true}
+          src="/images/saus.webp"
+          alt="Sausage"
+        />
+      </div>
+      <div className={sayingGenerator.buttonContainer}>
+        <Button color="gray" variant="outline" onClick={() => generateSaying()}>
+          <Text className={sayingGenerator.buttonText}> Generate Saying</Text>
+        </Button>
+      </div>
     </div>
   );
 };
